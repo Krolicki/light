8481729d55b7d79fca767aed7b57517557ecc88e
Krolicki <jakub.krolicki.k@gmail.com>
2022-04-25 22:07:43 +0200

    Wywoyłanie czasowe cd.

--- static/script.js
+++ static/script.js
@@ -1,12 +1,15 @@
-const decBtn = document.querySelector("#dec");
-const incBtn = document.querySelector("#inc");
+//const decBtn = document.querySelector("#dec");
+//const incBtn = document.querySelector("#inc");
 const timeVal = document.querySelector("#timeValue");
 const timerToOn = document.querySelector("#timerToOn");
 const timerToOff = document.querySelector("#timerToOff");
 const showTimer = document.querySelector("#showTimer");
+const timeSlider = document.querySelector("#timeSlider");
 const timerPanel = document.querySelector(".timerPanel");
+const minBtn = document.querySelector("#timerToMin");
+const secBtn = document.querySelector("#TimerToSec");
 		
-
+/*
 decBtn.addEventListener("click", function(){
 	if(timeVal.value>1)
 		timeVal.value--;
@@ -14,7 +17,7 @@ decBtn.addEventListener("click", function(){
 incBtn.addEventListener("click", function(){
 	if(timeVal.value<60)
 				timeVal.value++;
-});
+});*/
 timeVal.addEventListener("change", function(){
 	if(timeVal.value>60)
 		timeVal.value = 60;
@@ -22,13 +25,25 @@ timeVal.addEventListener("change", function(){
 		timeVal.value = 1;
 });
 timerToOn.addEventListener("click", function(){
-	timerToOn.classList.add("clicked");
-	timerToOff.classList.remove("clicked");
+	timerToOn.classList.add("turnOn");
+	timerToOff.classList.remove("turnOff");
 })
 timerToOff.addEventListener("click", function(){
-	timerToOn.classList.remove("clicked");
-	timerToOff.classList.add("clicked");
+	timerToOn.classList.remove("turnOn");
+	timerToOff.classList.add("turnOff");
 })
 showTimer.addEventListener("click", function(){
-	timerPanel.classList.add("showTmr");
+	timerPanel.classList.toggle("showTmr");
+})
+timeSlider.addEventListener("input", function(){
+	timeVal.value = timeSlider.value;
+	
+})
+timerToSec.addEventListener("click", function(){
+	timerToSec.classList.add("clicked");
+	timerToMin.classList.remove("clicked");
+})
+timerToMin.addEventListener("click", function(){
+	timerToSec.classList.remove("clicked");
+	timerToMin.classList.add("clicked");
 })
