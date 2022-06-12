7f4a623b1e41cacd57e3250791049f1d42eaa1d5
Krolicki <jakub.krolicki.k@gmail.com>
2022-06-12 22:28:09 +0200

    Drobne poprawki

--- static/timer.js
+++ static/timer.js
@@ -2,19 +2,22 @@ const timeToRefresh = document.querySelector("#timeToRefresh");
 
 if(timeToSwitch != null){
 	var now = new Date();
-	var timeLeft = parseInt((timeToSwitch.getTime() - now.getTime()) / 1000);
+	var timeLeft = parseInt((timeToSwitch.getTime() - now.getTime()) / 1000) -1;
 	var timeToReplace = timeLeft;
 	
 	document.getElementById("timeRefreshPanel").classList.remove("hide");
 	document.getElementById("showTimer").classList.add("hide");
 	if(timeLeft >= 0){
+			if(timeLeft <= 5){
+						timeToRefresh.style.color="red";
+					}
 			timeToRefresh.innerHTML = new Date(timeLeft * 1000).toISOString().substring(14, 19);
 			setInterval(function() {
 				if(timeLeft > 0){
 					now = new Date();
-					timeLeft = parseInt((timeToSwitch.getTime() - now.getTime()) / 1000);
+					timeLeft = parseInt((timeToSwitch.getTime() - now.getTime()) / 1000) -1;
 					timeToRefresh.innerHTML = new Date(timeLeft * 1000).toISOString().substring(14, 19);
-					if(timeLeft == 5){
+					if(timeLeft <= 5 && timeToRefresh.style.color != "red"){
 						timeToRefresh.style.color="red";
 					}
 				}
